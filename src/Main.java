aa99dd52b3078a2e80a0337b4d9492540dfebd4a
EnzoMarco01 <emarco@uade.edu.ar>
2024-06-04 21:36:38 -0300

    todo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import clases.Jugador;
+import gui.CrearJugador;
+
+import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Jugador p = new Jugador("enzo", "marco", 22, 69696969, "arg", "rendez");
-        p.i();
+        JFrame ventana = new JFrame("simple tetris");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+
+        CrearJugador cj = new CrearJugador();
+        ventana.add(cj);
+        //el tamaño de la ventana pasa a ser el de la interfaz crearJugador
+        ventana.pack();
+//        drawString = crea texto
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+
+
     }
 }
---
+++ src/gui/CrearJugador.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CrearJugador extends JPanel {
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public CrearJugador() {
+
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+//        this.setBackground(Color.BLACK);
+        this.setLayout(null);
+    }
+
+}
